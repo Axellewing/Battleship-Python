9b2570086d03ab2fa317cda01a38228c7ed40aba
Axellewing <127880600+Axellewing@users.noreply.github.com>
2023-08-13 18:29:10 +0200

    First try of printing function
    first try

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 class Board:
     board = []
     def __init__(self, size):
-        self.board = [[' ' for _ in range(size)] for _ in range(size)]
+        self.board = [['_' for _ in range(size)] for _ in range(size)]
+        self.size = size
+    
+    def print_board(self):
+        column = "   "
+        # row = ""
+        for x in range(1,self.size+1):
+            column += f"{x} "
+        print(column)
 
 if __name__ == "__main__":
-    SIZE = input()
+    SIZE = int(input())
     player_board = Board(SIZE)
-    print(player_board.board)
+    print(player_board.print_board())
