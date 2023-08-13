845ef2096c2b0e8b8743926e6f97c3ee4eea9dc3
Axellewing <127880600+Axellewing@users.noreply.github.com>
2023-08-13 18:53:02 +0200

    Board printing function done
    printing done

--- run.py
+++ run.py
@@ -6,12 +6,17 @@ class Board:
     
     def print_board(self):
         column = "   "
-        # row = ""
         for x in range(1,self.size+1):
             column += f"{x} "
         print(column)
+        for x in range(self.size):
+            row = f"{x+1}  "
+            for y in range(self.size):
+                row += f"{self.board[x][y]} "
+            print(row)
 
 if __name__ == "__main__":
     SIZE = int(input())
+    # 5 - 9
     player_board = Board(SIZE)
     print(player_board.print_board())
