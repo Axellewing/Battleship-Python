9d9de938fa25a44852600252d6f2f13b8b90f26b
Axellewing <127880600+Axellewing@users.noreply.github.com>
2023-08-13 19:08:18 +0200

    Create check_all_inputs function and done PEP8 test
    creating all inputs and PEP8

--- run.py
+++ run.py
@@ -1,22 +1,46 @@
 class Board:
     board = []
+
+
     def __init__(self, size):
         self.board = [['_' for _ in range(size)] for _ in range(size)]
         self.size = size
-    
+
+
     def print_board(self):
         column = "   "
-        for x in range(1,self.size+1):
+        for x in range(1, self.size + 1):
             column += f"{x} "
         print(column)
         for x in range(self.size):
-            row = f"{x+1}  "
+            row = f"{x + 1}  "
             for y in range(self.size):
                 row += f"{self.board[x][y]} "
             print(row)
 
+
+def check_all_inputs(size_check = False, size = 0):
+    ok = "n"
+    
+    while ok == "n":
+        try:
+            check = int(input())
+            if size_check and check > 0 and check < 10:
+                ok = "y"
+            elif size_check and check >= 10:
+                print("The description says no more than 10, GOT IT")
+            elif check > 0 and check <= size:
+                ok = "y"
+            else: 
+                print("incorrect number, try again man")
+        except:
+            print("did you see the description, or not :)")
+
+    return check           
+
+
 if __name__ == "__main__":
-    SIZE = int(input())
+    SIZE = check_all_inputs(True)
     # 5 - 9
     player_board = Board(SIZE)
-    print(player_board.print_board())
+    player_board.print_board()
